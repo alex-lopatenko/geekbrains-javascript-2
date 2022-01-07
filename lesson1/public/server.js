fa0f9f0d0007822e816e7bc7386806c160bbcf77
Alexey <alexey-lopatenko@yandex.ru>
2022-01-07 22:12:01 +0300

    Added lesson 1

---
+++ lesson1/public/server.js
@@ -0,0 +1,16 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+  console.log(req.url)
+  const body = req.url === '/styles.css'
+    ? fs.readFileSync('./styles.css')
+    : fs.readFileSync('./index.html')
+  res.end(body)
+})
+
+
+const port = process.env.PORT || 3000
+server.listen(port)
+
+console.log(`Server started on port ${port}!`)
